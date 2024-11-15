f36933cbd82adede6e5bf6d76cd89433dbecce51
janakaprasadq <janakaprasad.style@gmail.com>
2024-11-15 23:45:17 +0530

    imported bootstrap javascript file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
